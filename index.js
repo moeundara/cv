29920e2715bca93ea3157a11995bce35d5ddd0e7
moeundara <64655098+moeundara@users.noreply.github.com>
2020-12-20 18:43:38 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
+var numOfDrum = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numOfDrum; i++) {
 
-var randomImg1= Math.floor(Math.random()*6)+1;
-var imgDice1="dice"+randomImg1;
-var srcImg1="images/"+imgDice1+".png";
-
-var randomImg2=Math.floor(Math.random()*6)+1;
-var imgDice2="dice"+randomImg2;
-var srcImg2="images/"+imgDice2+".png";
-
-document.querySelectorAll("img")[0].setAttribute("src",srcImg1);
-document.querySelectorAll("img")[1].setAttribute("src",srcImg2);
-if(randomImg1>randomImg2){
-  document.querySelector("h1").innerHTML="⛳️Player1 Wins!";
-}else if(randomImg1<randomImg2){
-  document.querySelector("h1").innerHTML="⛳️Player2 Wins!";
-}else{
-  document.querySelector("h1").innerHTML="Draw!";
+  document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
+    var buttonClick = this.innerHTML;
+    makeSound(buttonClick);
+  });
 }
+document.addEventListener("keypress",function(event){
+makeSound(event.key);
+});
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    default:
+      console.log(key);
+
+  }
+}
+
+
+// var audio = new Audio("sounds/tom-1.mp3")
+// audio.play();
